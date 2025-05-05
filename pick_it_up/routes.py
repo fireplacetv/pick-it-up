5d00d43d8a53da2bced18a51888c7156dc9c57fe
Derrick Low <derrick@sonic.net>
2025-05-05 14:29:28 -0700

    working buttons!

--- pick_it_up/routes.py
+++ pick_it_up/routes.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    return "Pick-It-Up is running."
+    return render_template("index.html")
 
 @bp.route("/notify", methods=["POST"])
 def notify():
     data = request.json
-    apt = data.get("unit_number")
+    unit_number = data.get("unit_number")
     # TODO: insert notification logic here
-    print(f"Notify resident in Unit {apt}")
-    return jsonify({"status": "success", "message": f"Notificaiton sent to {apt}."})
+    print(f"TODO: Notify resident in {unit_number}")
+
+    return jsonify({"status": "success", "message": f"Notificaiton sent to {unit_number}."})
 
